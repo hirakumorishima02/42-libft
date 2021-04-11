d77ef913c794577763d622fde6d0a52ed1d1076c
Hiraku Morishima <hmorishi@c1r9s1.42tokyo.jp>
2021-04-12 06:30:24 +0900

    edit ft_atoi

--- itoa/ft_itoa.c
+++ itoa/ft_itoa.c
@@ -6,43 +6,50 @@
 /*   By: hmorishi <hmorishi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 08:42:15 by hmorishi          #+#    #+#             */
-/*   Updated: 2021/04/09 09:24:31 by hmorishi         ###   ########.fr       */
+/*   Updated: 2021/04/12 06:30:12 by hmorishi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int cul_digit(int n)
+static void	cul_len_digit(int n, int *digit, int *len)
 {
-	int digit;
-
-	digit = 1;
-	while (n >= 10)
+	*digit = 1;
+	*len = 0;
+	if (n <= 0)
 	{
-		n /= 10;
-		digit++;
+		*len = 1;
+		n = -n;
 	}
-	return (digit);
+	while (n / *digit > 9)
+	{
+		*digit *= 10;
+		*len += 1;
+	}
 }
 
 char *ft_itoa(int n)
 {
 	int digit;
-	int tmp;
+	int len;
 	int i;
-	char rep;
+	char *rep;
 
-	digit = cul_digit(n);
-	rep = (char *)malloc(sizeof(char) * digit + 1);
-	tmp = n;
+	cul_len_digit(n, &digit, &len);
+	rep = (char *)malloc(sizeof(*rep) * (len + 1));
 	i = 0;
-	while (digit)
+	if (n < 0)
 	{
-		rep[i] = tmp % 10 + '0';
-		tmp /= 10;
-		digit--;
+		rep[i] = '-';
 		i++;
+	}
+	if (n > 0)
+		n = -n;
+	while (digit > 0)
+	{
+		rep[i++] = -(n / digit % 10) + '0';
+		digit /= 10;
 	}
 	rep[i] = '\0';
 	return (rep);
@@ -50,10 +57,9 @@ char *ft_itoa(int n)
 
 int main()
 {
-	printf("%s", ft_itoa(12345));
+	printf("%s", ft_itoa(-12345));
 	return (0);
 }
-
 
 /*
 ** (malloc (3) で) を割り当て、文字列を返します。
